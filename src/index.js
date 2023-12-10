e2879273291b9ba08cc4f41858eefddbb4ac4eb8
Azizbek Akmatov <azakmatov@gmail.com>
2023-12-10 17:13:06 +0600

    complete project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let $ = require('jquery');
+let str = require('./constant');
+
+let App = function() {
+    console.log(str);
+    $('body').html(str);
+};
+
+let app = new App();
+
+app.get = () => {};
